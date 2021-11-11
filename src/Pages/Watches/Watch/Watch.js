df304efbc83cbf4eb358a18409dac0cb64bac0b1
rakibtweets <rakibhasanroky0@gmail.com>
2021-11-12 05:01:33 +0600

    watch card style

--- src/Pages/Watches/Watch/Watch.js
+++ src/Pages/Watches/Watch/Watch.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Card, Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './Watch.css';
 
 const Watch = (props) => {
-  const { _id, modelName, watchImg, pricing } = props.watch;
+  const { _id, modelName, watchImg, pricing, availability } = props.watch;
   return (
     <Col className=" d-block d-lg-flex">
-      <Card>
-        <Card.Img className=" img-fluid" variant="top" src={watchImg} />
+      <Card className="watch-card">
+        <Card.Img className="card-img" src={watchImg} />
         <Card.Body className=" text-center py-3 ">
-          <h5>{modelName}</h5>
+          <h5 className=" text-success fw-bold">{modelName}</h5>
           <h5 className=" fw-bold"> $ {pricing}</h5>
+          <h5 className=" fw-bold text-secondary"> {availability}</h5>
         </Card.Body>
         <div className=" d-flex justify-content-around align-content-center mb-3">
           <Link to={`/watch/${_id}`}>
